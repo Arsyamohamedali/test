01e6b61a7b1a95fb48a9851e7dbaf8beb362128b
Arsyamohamedali <70328502+Arsyamohamedali@users.noreply.github.com>
2021-05-03 12:34:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,25 +9,25 @@ import numpy as np
 from PIL import Image,ImageOps 
 
 def app():
-	new_model = keras.models.load_model("haemorrhage_modelnorm.h5")
-	st.title("Haemorrhage Detection")
+	new_model = keras.models.load_model("modelpncnn.h5")
+	st.title("Pneumonia Detection")
 	
 	uploaded_file_hem = st.file_uploader("Choose a image file", type=['png','jpg','jpeg'])
 	if uploaded_file_hem is not None:
 		image = Image.open(uploaded_file_hem)
-		size =(128,128)
+		size =(150,150)
 		image1=image.resize(size)
 		image1=ImageOps.grayscale(image1)
 		x=np.asarray(image1)
 		x=np.expand_dims(x,axis=0)
-		x=np.reshape(x,(1,128,128,1))
+		x=np.reshape(x,(-1,150,150,1))
 		x=x/255.0
 		if(new_model.predict_classes(x)[0][0]==1):
-			st.write('***Haemorrhage***')
+			st.write('***Normal***')
 			with st.beta_expander('Show Image'):
 				st.image(image,channels='BGR',width=200)
 		else:
-			st.write('***Normal***')
+			st.write('***Pneumonia***')
 			with st.beta_expander('Show Image'):
 				st.image(image,channels='BGR',width=200)
 				
